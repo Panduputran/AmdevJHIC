2a6e1580dd9654719070f390962ebf33b947c9df
Panduputran <pamediaid@gmail.com>
2025-10-02 21:29:22 +0700

    public-side-added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,5 @@
 @section('title', 'Halaman Utama')
 
 @section('content')
-    <div class="p-10">
-        <h1 class="text-2xl font-bold">Selamat Datang di SMK Amaliah</h1>
-    </div>
+    
 @endsection
